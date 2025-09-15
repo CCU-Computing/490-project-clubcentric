34ce48b1b5dffaf63ea81932e918fe3ae92a6cb6
Lauren <leharman@coastal.edu>
2025-09-14 21:04:36 -0400

    Added BrowserRouter and changed code in main.jsx file to use it. JSON files with packages also updated.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router"; // BrowserRouter allows for multiple pages
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* Wrap entire app with BrowserRouter */}
+        <App />
+    </BrowserRouter>
   </StrictMode>,
 )
