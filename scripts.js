a3f96b9f81f06b575aa5ae32e3f782d6d71c5c11
Marcos Carvajal <mcarvaja@coastal.edu>
2025-09-22 14:02:54 -0400

    Revert "Added my temporary user story files"
    This reverts commit 20d69ac0fc02c6545b76e9066d9ed2281184f192.

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function toggleBox(element) {
-    const details = element.querySelector('.club-details');
-    const isVisible = details.style.display === "block";
-
-    // Toggle visibility
-    details.style.display = isVisible ? "none" : "block";
-}
