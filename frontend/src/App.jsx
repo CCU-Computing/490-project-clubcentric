763a9180eca61d9dd302dd77608dd3f98b031dbf
Marcos Carvajal <mcarvaja@coastal.edu>
2025-10-31 16:02:02 -0400

    Fixed import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from './components/navbar/Navbar' // Navigation Bar
 import ProtectedRoute from './components/auth/AuthProvider';
+import ClubSearch from './components/ClubSearch';
 
 function App() {
 
